9f91bdfed632a2a39d3561e21c7a8357eb66bb07
IvanDamNation <ivan.suvalkin@gmail.com>
2022-01-08 18:39:27 +0300

    Add a few non-builtin exceptions

--- game_objects.py
+++ game_objects.py
@@ -1,5 +1,24 @@
 # file contains objects definition for Battleships game
 
+# Exceptions section
+class BoardException(Exception):
+    pass
+
+
+class OutOfRangeException(BoardException):
+    def __str__(self):
+        return 'Coordinates out of board range.'
+
+
+class UsedCoordinatesException(BoardException):
+    def __str__(self):
+        return 'You already shoot on this coordinate.'
+
+
+class WrongShipPlacement(BoardException):
+    pass
+
+
 # base object
 class Dot:
     def __init__(self, x_coord, y_coord):
