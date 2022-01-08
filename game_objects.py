78327d9ef429f392c2622440739780e0ca0b4022
IvanDamNation <ivan.suvalkin@gmail.com>
2022-01-08 21:10:52 +0300

    Add ship object

--- game_objects.py
+++ game_objects.py
@@ -30,3 +30,32 @@ class Dot:
 
     def __repr__(self):  # Print shortcut
         return f'Dot({self.x}, {self.y})'
+
+
+# Constructing ship with dot objects
+class Ship:
+    def __init__(self, bow, length, orientation):
+        self.bow = bow
+        self.length = length
+        self.orient = orientation
+        self.hp = length
+
+    @property
+    def ship_dots(self):
+        ship_placement = []
+        for hull in range(self.length):
+            current_x = self.bow.x
+            current_y = self.bow.y
+
+            if self.orient == 0:
+                current_x += hull
+
+            elif self.orient == 1:
+                current_y += hull
+
+            ship_placement.append(Dot(current_x, current_y))
+
+        return ship_placement
+
+    def get_hit(self, shot):
+        return shot in self.ship_dots
