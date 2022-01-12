516e035d9886c51f1849d888f5c100f0c7a57ded
IvanDamNation <ivan.suvalkin@gmail.com>
2022-01-12 22:58:14 +0300

    Make import section and started MainObject class.

--- main_gamefile.py
+++ main_gamefile.py
@@ -12,25 +12,62 @@
 
 
 # Import section
-# import random
+from random import randint
 
-# import game_objects
-
-
-def main():
-    pass
+from game_objects import BoardException, OutOfRange, UsedCoordinates, \
+    WrongShipPlacement
+from game_objects import Dot, Ship, GameField, PlayerMain, Human, AI
 
 
-def rules():
-    print('Welcome to the Battleships game!')
-    print('The rules are standard.')
-    print('Input X and Y coordinates for shoot.')
-    print("Try to hit all opponent's ships on map")
-    print('before opponent destroy all yours.')
-    print('Your opponent will be simple AI.')
-    print('Good luck!')
+class MainGameObject:
+    def __init__(self, width=6):
+        self.width = width
+        player_board = self.make_board_forced()
+        computer_board = self.make_board_forced()
+        computer_board.hidden = True
+
+        self.ai = AI(computer_board, player_board)
+        self.user = Human(player_board, computer_board)
+
+    def make_board(self):  # TODO Make ship lengths changeable
+        lengths = [3, 2, 2, 1, 1, 1, 1]
+        board = GameField()
+        attempt_count = 0
+        for taken_length in lengths:
+            while True:
+                attempt_count += 1
+                if attempt_count > 2000:
+                    return None
+                ship = Ship(Dot(randint(0, self.width),
+                                randint(0, self.width)),
+                            taken_length, randint(0, 1))
+                try:
+                    board.add_ship(ship)
+                    break
+                except WrongShipPlacement:
+                    pass
+        board.clean_used_dots()
+        return board
+
+    def make_board_forced(self):
+        board = None
+        while board is None:
+            board = self.make_board()
+        return board
+
+    def rules(self):
+        print('Welcome to the Battleships game!')
+        print('--------------------------------------')
+        print('The rules are standard.')
+        print('Input X and Y coordinates for shoot.')
+        print("Try to hit all opponent's ships on map")
+        print('before opponent destroy all yours.')
+        print('Your opponent will be simple AI.')
+        print('--------------------------------------')
+        print('Good luck!')
 
 
 # Starting section
 if __name__ == '__main__':
-    main()
+    game = MainGameObject()
+    # print(game.make_board_forced())
